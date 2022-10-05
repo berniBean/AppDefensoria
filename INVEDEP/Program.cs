0c20750fa7dd7b397c1523686d59f95ca8a2839c
berniBean <ernesto_fope55@hotmail.com>
2022-10-05 14:51:20 -0500

    agregando conexion DI

--- INVEDEP/Program.cs
+++ INVEDEP/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
 namespace INVEDEP
 {
     internal static class Program
@@ -13,5 +16,38 @@ namespace INVEDEP
             ApplicationConfiguration.Initialize();
             Application.Run(new Form1());
         }
+
+        static IServiceProvider BuildServiceProvider()
+        {
+            IServiceCollection collection = new ServiceCollection();
+            
+            #region Configuracion de connexion JSON
+            IConfiguration configuration = new ConfigurationBuilder()
+            .AddJsonFile("appSettings.json", optional: false)
+            .Build();
+
+            IConfig config = configuration.Get<Config>();
+            collection.AddSingleton<IConfig>(config);
+            #endregion
+
+
+            #region Connexion  con bases de DAtos mysql y Dapper
+            collection.AddOptions();
+
+            collection.Configure<ConexionConfiguracion>(configuration.GetSection("ConnectionStrings"));
+
+            collection.AddDbContext<sim_verformsContext>(opt => {
+                opt.UseMySQL(configuration.GetConnectionString("SefiplanConnection"))
+                .LogTo(Console.WriteLine, new[] { DbLoggerCategory.Database.Command.Name }
+                    , Microsoft.Extensions.Logging.LogLevel.Information);
+
+
+            });
+
+            #endregion
+            return collection.BuildServiceProvider();
+        }
     }
+
+
 }
